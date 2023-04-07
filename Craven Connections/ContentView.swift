8006619597cc73009407231fb0fdc633681cc951
shawnweiker <shawn@craveitarts.com>
2023-04-07 14:27:49 -0400

    Initial Commit

---
+++ Craven Connections/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Craven Connections
+//
+//  Created by Craven on 4/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
