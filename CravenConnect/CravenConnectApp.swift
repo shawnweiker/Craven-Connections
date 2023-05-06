15cccee6577b1d2ddb4441aa8ec0ec2474c838d0
shawnweiker <shawn@craveitarts.com>
2023-05-06 19:24:48 -0400

    Initial Commit

---
+++ CravenConnect/CravenConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CravenConnectApp.swift
+//  CravenConnect
+//
+//  Created by Craven on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CravenConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
